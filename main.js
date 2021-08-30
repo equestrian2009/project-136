dfcb7a2dca61c629316da511c60d29d2157bd71f
equestrian2009 <87225010+equestrian2009@users.noreply.github.com>
2021-08-30 09:31:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status="";
+video="";
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,480,380);
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status : detecting objects";
+    object_name=document.getElementById("object_name").value;
+
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status=true;
+}
